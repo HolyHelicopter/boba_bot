09e9105312f800dc8e9efeb555fb52e302141a3e
Markunec <holyhelicopter@yandex.ru>
2022-03-17 03:33:32 +0400

    initial

---
+++ ygo_tg_bot/constants.py
@@ -0,0 +1,2 @@
+TG_BOT_TOKEN = '5156402837:AAEYFaX_FowRdMSsSBUuxpTGxV1bHFZ5Y9g'
+TG_URL = "https://api.telegram.org/bot{}/".format(TG_BOT_TOKEN)
---
+++ ygo_tg_bot/views/bot_handler.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+import requests
+import json
+
+from ygo_tg_bot.constants import TG_URL
+
+
+def bot_handler(request):
+    message = json.dumps(request.POST)
+
+    requests.post(
+        '{}/sendMessage'.format(TG_URL),
+        {
+            'chat_id': '-601053432',
+            'text': message
+        }
+    )
+
+    return HttpResponse(200)

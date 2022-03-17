9e9f02cb1f9b3e181a0183bd42d11669e7dcb23e
Markunec <holyhelicopter@yandex.ru>
2022-03-17 06:57:39 +0400

    json

--- ygo_tg_bot/views/bot_handler.py
+++ ygo_tg_bot/views/bot_handler.py
@@ -8,7 +8,7 @@ from ygo_tg_bot.constants import TG_URL
 
 @csrf_exempt
 def bot_handler(request):
-    message = dict(QueryDict(request.body))['update_id']
+    message = str(dict(QueryDict(request.body))['update_id'])
 
     requests.post(
         '{}sendMessage'.format(TG_URL),

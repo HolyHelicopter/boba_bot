b17043824bfb57df1d4bb9db2d10e586fca7ce13
Markunec <holyhelicopter@yandex.ru>
2022-03-17 06:38:41 +0400

    csrf exempt

--- ygo_tg_bot/views/bot_handler.py
+++ ygo_tg_bot/views/bot_handler.py
@@ -1,19 +1,21 @@
 from django.http import HttpResponse
 import requests
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 from ygo_tg_bot.constants import TG_URL
 
 
+@csrf_exempt
 def bot_handler(request):
     message = json.dumps(request.POST)
 
-    # requests.post(
-    #     '{}sendMessage'.format(TG_URL),
-    #     {
-    #         'chat_id': '-601053432',
-    #         'text': message
-    #     }
-    # )
+    requests.post(
+        '{}sendMessage'.format(TG_URL),
+        {
+            'chat_id': '-601053432',
+            'text': message
+        }
+    )
 
     return HttpResponse(200)

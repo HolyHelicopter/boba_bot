74ba4447ab23fc36b558665b6c70dd9068fed923
Markunec <holyhelicopter@yandex.ru>
2022-03-18 05:34:55 +0400

    frog

---
+++ ygo_tg_bot/functions/frog.py
@@ -0,0 +1,93 @@
+import requests
+import random
+
+from ygo_tg_bot.constants import TG_URL
+
+BING_URL = "https://bing-image-search1.p.rapidapi.com/images/search?q="
+BING_HEADERS = {
+    "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+    "x-rapidapi-key": "fb3dd38f00msh738d9f9b25b29acp13d692jsn8859445253e3",
+    "useQueryString": 'true'
+}
+BACKUP_QUERIES = [
+    'лягушка мем',
+    'лягушка косплей',
+    'лягушка сказка',
+    'лягушка нарисованная',
+    'лягушка смешная',
+    'лягушка кермит'
+]
+
+
+def frog(update_data):
+    if 'message' in update_data and 'message_id' in update_data['message'] and 'text' in update_data['message'] \
+            and 'chat' in update_data['message'] and 'id' in update_data['message']['chat']:
+
+        try:
+            message_text = update_data['message']['text']
+
+            lowercase_text = message_text.replace('К', 'к').replace('В', 'в').replace('А', 'а')
+
+            if 'квак' in lowercase_text:
+                words = message_text.split()
+
+                kvak_found = False
+                words_temp = []
+                for word in words:
+                    word_formatted = word.replace('К', 'к').replace('В', 'в').replace('А', 'а')
+                    if word_formatted == 'квак':
+                        kvak_found = True
+                    else:
+                        words_temp.append(word)
+                words = words_temp
+
+                if kvak_found:
+                    query = 'лягушка'
+                    for word in words:
+                        query += ' ' + word
+
+                    found_images = requests.get(
+                        BING_URL + query,
+                        headers=BING_HEADERS
+                    ).json()['value']
+
+                    if not len(found_images):
+                        backup_index = random.randint(0, len(BACKUP_QUERIES) - 1)
+                        backup_query = BACKUP_QUERIES[backup_index]
+                        found_images = requests.get(
+                            BING_URL + backup_query,
+                            headers=BING_HEADERS
+                        ).json()['value']
+
+                    if len(found_images):
+                        image_index = random.randint(0, len(found_images) - 1)
+                        image_url = found_images[image_index]['contentUrl']
+
+                        message_id = update_data['message']['message_id']
+                        chat_id = update_data['message']['chat']['id']
+
+                        try:
+                            requests.post(
+                                '{}sendPhoto'.format(TG_URL),
+                                {
+                                    'chat_id': chat_id,
+                                    'photo': image_url,
+                                    'reply_to_message_id': message_id
+                                }
+                            )
+                        except Exception as e:
+                            image_index = random.randint(0, len(found_images) - 1)
+                            image_url = found_images[image_index]['contentUrl']
+                            try:
+                                requests.post(
+                                    '{}sendPhoto'.format(TG_URL),
+                                    {
+                                        'chat_id': chat_id,
+                                        'photo': image_url,
+                                        'reply_to_message_id': message_id
+                                    }
+                                )
+                            except Exception as e:
+                                pass
+        except Exception:
+            pass
--- ygo_tg_bot/views/bot_handler.py
+++ ygo_tg_bot/views/bot_handler.py
@@ -3,6 +3,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 
 from ygo_tg_bot.functions.get_card import get_card
+from ygo_tg_bot.functions.frog import frog
 
 
 @csrf_exempt
@@ -10,5 +11,7 @@ def bot_handler(request):
     update_data = json.loads(request.body)
 
     get_card(update_data)
+
+    frog(update_data)
 
     return HttpResponse(200)

59090a6fdbad176cc5d427722cd3fd8871f0b448
img <holyhelicopter@yandex.ru>
2023-06-18 09:09:29 +0300

    for onrender hosting

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&us&y@u01^302f@!l(u%g85n=*4a3#j$cz@$_t33vqna($5or6'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='&us&y@u01^302f@!l(u%g85n=*4a3#j$cz@$_t33vqna($5or6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

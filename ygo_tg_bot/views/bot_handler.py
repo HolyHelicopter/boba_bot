1a61c5c9485f40ec77621a1573c81ad15920589e
img <holyhelicopter@yandex.ru>
2023-06-18 09:57:00 +0300

    for onrender hosting

--- ygo_tg_bot/views/bot_handler.py
+++ ygo_tg_bot/views/bot_handler.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 
-from ygo_tg_bot.functions.get_card import get_card
 from ygo_tg_bot.functions.frog import frog
 
 

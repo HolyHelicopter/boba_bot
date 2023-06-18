f6d0319a0840953908de5d1ef0d466f5c8c100bd
img <holyhelicopter@yandex.ru>
2023-06-18 09:36:30 +0300

    for onrender hosting

--- ygo_tg_bot/constants.py
+++ ygo_tg_bot/constants.py
@@ -1,4 +1,4 @@
-TG_BOT_TOKEN = '6280273255:AAGJnbxdrTFiwCuU3dSXMk40P_JMJkLEbWg'
+TG_BOT_TOKEN = '2129955577:AAGdJFNFQ5PaNNRpBkqc0BlOQubiUBU3Q7Y'
 TG_URL = "https://api.telegram.org/bot{}/".format(TG_BOT_TOKEN)
 
 RUSSIAN_ALPHABET = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя'
